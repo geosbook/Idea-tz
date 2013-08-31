5e2ca8afff56c38313dc81b860fc6ddc156316f1
Antonio <abiallo@gmail.com>
2013-08-31 18:54:59 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeaTz::Application.config.secret_token = '365f6c3126c532de130805ae2f817bcd71c6681c127d5b4896b4b8690b6640c8644d7718783c5f55f5fbc32fd24ef7f8f00cc5ff5ba2cefe989910cae8f1d578'
