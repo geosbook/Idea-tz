165577fc4c652aabc6043ba83d567fc4c2ec2a6d
Antonio <abiallo@gmail.com>
2013-09-04 00:02:30 +0400

    added full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,14 +4,19 @@ describe "Static pages" do
 
    describe "Home page" do
 
+      it "should have  the base title " do
+         visit '/static_pages/home'
+         page.should have_selector('title', :text => "Idea-fz Sample App")
+      end
+
+      it "should not have  a custom page title " do
+         visit '/static_pages/home'
+         page.should_not have_selector('title', :text => " - Home")
+      end
+
       it "should have  the h1 'Sample App'" do
          visit '/static_pages/home'
          page.should have_selector('h1', :text => 'Sample App')
-      end
-
-      it "should have  the title 'Home'" do
-         visit '/static_pages/home'
-         page.should have_selector('title', :text => 'Idea-tz Sample App | Home')
       end
 
    end
@@ -25,7 +30,7 @@ describe "Help page" do
 
   it "should have  the title 'Help'" do
     visit '/static_pages/help'
-    page.should have_selector('title', :text=>'Idea-tz Sample App | Help')
+    page.should have_selector('title', :text => "#{base_title} - Help")
   end
 
 end
@@ -39,9 +44,20 @@ describe "About page" do
 
   it "should have  the title 'About Us'" do
     visit '/static_pages/about'
-    page.should have_selector('title', :text => 'Idea-tz Sample App | About Us')
+    page.should have_selector('title', :text => "#{base_title} - About Us")
   end
 
 end
+
+   describe "Contact page" do
+
+      it "should have  the title 'Contact'" do
+         visit '/static_pages/contact'
+         page.should have_selector('title', :text => "#{base_title} - Contact")
+      end
+
+   end
+
+
 
 end
