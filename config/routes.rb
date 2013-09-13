6fe397eb0b7f0a6863661686886a6ebd46be2476
Antonio <abiallo@gmail.com>
2013-09-13 09:36:30 +0400

    Finish Sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 IdeaTz::Application.routes.draw do
   resources :users
-#  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
 
